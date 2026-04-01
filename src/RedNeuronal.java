235c762f2802b064ae2adb5a08f261bca517a89e
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-01 11:13:32 -0500

    commit - RedNeuronal / .

---
+++ src/RedNeuronal.java
@@ -0,0 +1,15 @@
+public class RedNeuronal {
+
+    // simulacion de neurona ( suma ponderada ) .
+    public static double calcularSuma(double[] entradas) {
+        double suma = 0;
+        for (double valor : entradas) {
+            suma += valor; // peso=1 .
+        }
+        return suma;
+    }
+
+    public static double calcularPromedio(double suma) {
+        return suma / 5.0;
+    }
+}   
